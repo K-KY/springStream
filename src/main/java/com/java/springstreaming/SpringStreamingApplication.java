b16efd3e64ed23ef4c20f59181d5798b58764f49
K-KY <kyuyoungk@naver.com>
2025-09-07 02:56:01 +0900

    Init

---
+++ src/main/java/com/java/springstreaming/SpringStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.java.springstreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStreamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStreamingApplication.class, args);
+    }
+
+}
