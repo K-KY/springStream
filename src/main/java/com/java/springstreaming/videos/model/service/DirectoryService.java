59df73a58db9839e7a772ca6e88209933ad4563d
K-KY <kyuyoungk@naver.com>
2025-09-23 22:26:54 +0900

    Feat : 디렉터리 탐색 서비스

---
+++ src/main/java/com/java/springstreaming/videos/model/service/DirectoryService.java
@@ -0,0 +1,33 @@
+package com.java.springstreaming.videos.model.service;
+
+import com.java.springstreaming.videos.model.file_manager.FileManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.Path;
+import java.util.List;
+
+@Log4j2
+@RequiredArgsConstructor
+@Service
+public class DirectoryService {
+
+    private final FileManager fileManager;
+
+    public List<Path> readFiles() {
+        return fileManager.readFiles();
+    }
+
+    public List<Path> readFiles(String path) {
+        return fileManager.readFiles(path);
+    }
+
+    public List<Path> readDirs() {
+        return fileManager.readDirs();
+    }
+
+    public List<Path> readDirs(String path) {
+        return fileManager.readDirs(path);
+    }
+}
