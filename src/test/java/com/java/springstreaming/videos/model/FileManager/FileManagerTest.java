d44ca7e25f31ffeae81f3c6b2819f53f4a20d921
K-KY <kyuyoungk@naver.com>
2025-09-23 22:24:54 +0900

    Test : 클래스 로직 변경으로 테스트 변경

--- src/test/java/com/java/springstreaming/videos/model/FileManager/FileManagerTest.java
+++ src/test/java/com/java/springstreaming/videos/model/FileManager/FileManagerTest.java
@@ -8,16 +8,17 @@ import java.nio.file.Path;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class FileManagerTest {
-
-    private final FileManager fileManager = FileManager.getInstance();
+    private final FileManager fileManager
+            = new FileManager("src/main/java/com/java/springstreaming/videos/model/file_manager");
     @Test
     @DisplayName("지정한 경로의 \"파일\"을 전부 읽는다.")
     public void fileReadTest() {
         //테스트의 성공을 보장해야 하므로 테스트가 실행 되려면 존재 해야하는 파일로 검증
         List<Path> read = fileManager
-                .readFiles("src/main/java/com/java/springstreaming/videos/model/file_manager");
+                .readFiles();
         assertThat(read.contains(Path.of("FileManager.java"))).isTrue();
     }
 
@@ -25,7 +26,13 @@ public class FileManagerTest {
     @DisplayName("지정한 경로의 \"디렉터리\"를 전부 읽는다.")
     public void fileReadTest2() {
         List<Path> read = fileManager
-                .readDirs("src/main/java/com/java/springstreaming/videos/model");
+                .readDirs();
         assertThat(read.contains(Path.of("file_manager")));
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 경로 차단")
+    public void fileReadTest3() {
+        assertThatThrownBy(() ->fileManager.readDirs("../")).isInstanceOf(SecurityException.class);
     }
 }
