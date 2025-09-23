f1d63daf4d1af0f023efff23f6f265e4ffa63152
K-KY <kyuyoungk@naver.com>
2025-09-23 22:27:31 +0900

    Feat : 디렉토리 탐색 컨트롤러

--- src/main/java/com/java/springstreaming/videos/controller/VideoController.java
+++ src/main/java/com/java/springstreaming/videos/controller/VideoController.java
@@ -1,22 +1,65 @@
 package com.java.springstreaming.videos.controller;
 
-import com.java.springstreaming.videos.model.file_manager.FileManager;
+import com.java.springstreaming.videos.model.service.DirectoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Path;
 import java.util.List;
-import java.util.stream.Collectors;
 
+@Log4j2
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("videos")
 public class VideoController {
 
-    private static final FileManager fileManager = new FileManager();
+    private final DirectoryService directoryService;
 
-    @GetMapping
-    public List<String> getVideos() {
-        return fileManager.readFiles("src/main/java/com/java/springstreaming/videos/model/file_manager")
-                .stream().map(s -> s.toString()).collect(Collectors.toList());
+    @GetMapping("file")
+    public ResponseEntity<List<String>> getFileNames(String path) {
+        log.info("Request Reading files from {}", path);
+        try {
+            if (path != null && !path.isEmpty()) {
+                List<String> list = directoryService.readFiles(path)
+                        .stream().map(Path::toString).toList();
+                return getBody(list);
+
+            }
+            List<String> list = directoryService.readFiles()
+                    .stream().map(Path::toString).toList();
+            return getBody(list);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(List.of(e.getMessage()));
+        }
+    }
+
+    @GetMapping("dir")
+    public ResponseEntity<List<String>> getDirNames(String path) {
+        log.info("Request Reading dirs from {}", path);
+        try {
+            if (path != null && !path.isEmpty()) {
+                List<String> list = directoryService.readDirs(path)
+                        .stream().map(Path::toString).toList();
+                return getBody(list);
+
+            }
+                List<String> list = directoryService.readDirs()
+                        .stream().map(Path::toString).toList();
+                return getBody(list);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(List.of(e.getMessage()));
+        }
+    }
+
+    private static ResponseEntity<List<String>> getBody(List<String> list) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(list);
     }
 }
