1aa5a5ae2b9ed45b9996f4c96846d5e594810579
K-KY <kyuyoungk@naver.com>
2025-09-24 10:03:07 +0900

    Feat : cors 설정

---
+++ src/main/java/com/java/springstreaming/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.java.springstreaming.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Value("${local.front}")
+    private String frontAddress;
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // 모든 엔드포인트
+                        .allowedOrigins(frontAddress) // 로컬 프론트엔드
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 필요한 메서드
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // 쿠키/인증정보 포함 허용 시
+            }
+        };
+    }
+
+}
